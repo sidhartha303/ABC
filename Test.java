dc6f32e5459821871fe4e6b59bb55800885236ba
sidhartha303 <sidhartha303@gmail.com>
2018-03-30 18:05:11 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,60 +1,72 @@
-package map;
+package TreeSet;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.NavigableMap;
 import java.util.Set;
-import java.util.TreeMap;
+import java.util.Spliterator;
 import java.util.TreeSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Test{
+public class Test implements Runnable{
 
+	public void run() {
+        System.out.println(Thread.currentThread().getName());  		
+	}
+	/*private String message;  
+	    public  Test(String s){  
+	        this.message=s;  
+	   }  
+	     public void run() {  
+	        System.out.println(Thread.currentThread().getName()+" (Start) message = "+message);  
+	       processmessage();//call processmessage method that sleeps the thread for 2 seconds  
+	        System.out.println(Thread.currentThread().getName()+" (End)");//prints thread name  
+	    }  
+	    private void processmessage() {  
+	        try {  Thread.sleep(2000);  } catch (InterruptedException e) { e.printStackTrace(); }  
+	   }  */
+
+	
 	public static void main(String[] args) {
 
-		 Map<Integer, String> testMap = new TreeMap<Integer, String>(Collections.reverseOrder());
-	        testMap.put(10, "a");
-	        testMap.put(20, "b");
-	        testMap.put(30, "c");
-	        testMap.put(40, "d");
-   		for(Map.Entry m:testMap.entrySet()) {
-		System.out.println(m.getKey()+"-"+m.getValue());
-	       }
-   		
-		try {
-			FileOutputStream f=new FileOutputStream("D:\\ZZ.txt");
-			String s="Welcome to Galaxe";
-			byte[] b=s.getBytes();
-			f.write(b);
-			f.close();
-			System.out.println("Success");
-		}catch(Exception e) {
-			System.out.println(e);
-		}
+		/*Set<Integer> s=new TreeSet<>();
+		s.add(987);
+		s.add(9876);
+		s.add(543);
+		s.add(12);
+		Spliterator<Integer> a=s.spliterator();
+		a.forEachRemaining(System.out::println);
+	
+		Test t1=new Test();
+		Test t2=new Test();
+		Test t3=new Test();
+		t1.setDaemon(true);
+		    
+		 t1.start();  
+		 t2.start();  
+		 t3.start();  */
 		
-		try {
-			FileInputStream fin=new FileInputStream("D:\\ZZ.txt");
-			int i=0;    
-			while((i=fin.read())!=-1){    
-		    	  System.out.print((char)i);    
-		    }    
-			fin.close();
-		}catch(Exception e) {
-			System.out.println(e);
-		}
-		System.out.println();
-		HashMap<Integer, String> m=new HashMap<>();
-		m.put(1,"A");
-		m.put(2,"B");
-		m.put(3,"C");
-		
-		for(Map.Entry n:m.entrySet()) {
-			System.out.println(n.getKey()+"-"+n.getValue());
-		}
+		/*ExecutorService executor = Executors.newFixedThreadPool(5);//creating a pool of 5 threads  
+		       for (int i = 0; i < 10; i++) {  
+	           Runnable worker = new Test("" + i);  
+		           executor.execute(worker);//calling execute method of ExecutorService  
+	          }  
+	       executor.shutdown();  
+		       while (!executor.isTerminated()) {   }  
+		        System.out.println("Finished all threads");  
+*/		
+		/*Test runnable=new Test();
+		ThreadGroup tg1=new ThreadGroup("Parent ThreadGroup");
+		Thread t1 = new Thread(tg1, runnable,"one");  
+		          t1.start();  
+		          Thread t2 = new Thread(tg1, runnable,"two");  
+		          t2.start();  
+		          Thread t3 = new Thread(tg1, runnable,"three");  
+		          t3.start();  
+		              
+		          System.out.println("Thread Group Name: "+tg1.getName());  
+		          tg1.list();  */
 		
 		
-	}		
-}
+		
+		
+}	
+	}
